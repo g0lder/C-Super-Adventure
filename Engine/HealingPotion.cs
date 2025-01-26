ee4eb2d58dbe27491339871738e1d771cc6beefd
g0lder <gold49erz@gmail.com>
2025-01-25 20:39:58 -0600

    Update HealingPotion.cs

--- Engine/HealingPotion.cs
+++ Engine/HealingPotion.cs
@@ -1,63 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-using Engine;
+namespace Engine;
 
-
-namespace Super_Adventure
+public class HealingPotion : Item
 {
-    public partial class SuperAdventure : Form
+    public int AmountToHeal { get; set; }
+    
+    public HealingPotion(int id, string name, string namePlural, int amountToHeal)  :  base(id, name, namePlural)
     {
-        private Player _player;
-
-        public SuperAdventure()
-        {
-            InitializeComponent();
-
-            _player = new Player(currentHealth:10, maximumHealth:10, gold:20, xp:0, level:1);
-
-            lblHealth.Text = _player.CurrentHealth.ToString();
-            lblGold.Text = _player.Gold.ToString();
-            lblXP.Text = _player.XP.ToString();
-            lblLevel.Text = _player.Level.ToString();
-        }
-
-        private void btnNorth_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnEast_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnSouth_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnWest_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnUseWeapon_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnUsePotion_Click(object sender, EventArgs e)
-        {
-
-        }
-        
+        AmountToHeal = amountToHeal;
     }
 }
